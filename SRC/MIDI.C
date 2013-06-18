b2a67363d9bcc26daf940ce43386139cce220cb4
bjt <bjtayler@bjtayler.plus.com>
2013-06-18 10:03:29 +0100

    Release 1.0

---
+++ SRC/MIDI.C
@@ -0,0 +1,334 @@
+/*
+ *  Copyright (C) 2002-2013  The DOSBox Team
+ *  Copyright (C) 2013       bjt
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * ------------------------------------------
+ * SoftMPU by bjt - Software MPU-401 Emulator
+ * ------------------------------------------
+ *
+ * Based on original midi.c from DOSBox
+ *
+ */
+
+/* SOFTMPU: Moved exported functions & types to header */
+#include "export.h"
+
+/* SOFTMPU: Don't need these includes */
+/*#include <assert.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <string>
+#include <algorithm>
+
+#include "SDL.h"
+
+#include "dosbox.h"
+#include "cross.h"
+#include "support.h"
+#include "setup.h"
+#include "mapper.h"
+#include "pic.h"
+#include "hardware.h"
+#include "timer.h"*/
+
+/* SOFTMPU: Additional defines, typedefs etc. for C */
+typedef unsigned long Bit32u;
+typedef int Bits;
+
+#define SYSEX_SIZE 1024
+#define RAWBUF  1024
+
+static char* MIDI_welcome_msg="\xf0\x41\x10\x16\x12\x20\x00\x00    SoftMPU v1.0    \x2d\xf7"; /* SOFTMPU */
+
+static Bit8u MIDI_evt_len[256] = {
+  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x00
+  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x10
+  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x20
+  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x30
+  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x40
+  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x50
+  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x60
+  0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,  // 0x70
+
+  3,3,3,3, 3,3,3,3, 3,3,3,3, 3,3,3,3,  // 0x80
+  3,3,3,3, 3,3,3,3, 3,3,3,3, 3,3,3,3,  // 0x90
+  3,3,3,3, 3,3,3,3, 3,3,3,3, 3,3,3,3,  // 0xa0
+  3,3,3,3, 3,3,3,3, 3,3,3,3, 3,3,3,3,  // 0xb0
+
+  2,2,2,2, 2,2,2,2, 2,2,2,2, 2,2,2,2,  // 0xc0
+  2,2,2,2, 2,2,2,2, 2,2,2,2, 2,2,2,2,  // 0xd0
+
+  3,3,3,3, 3,3,3,3, 3,3,3,3, 3,3,3,3,  // 0xe0
+
+  0,2,3,2, 0,0,1,0, 1,0,1,1, 1,0,1,0   // 0xf0
+};
+
+
+static struct {
+	Bitu mpuport;
+	Bitu status;
+	Bitu cmd_len;
+	Bitu cmd_pos;
+	Bit8u cmd_buf[8];
+	Bit8u rt_buf[8];
+	struct {
+		Bit8u buf[SYSEX_SIZE];
+		Bitu used;
+		Bitu delay;
+		Bit32u start;
+	} sysex;
+	bool available;
+	/*MidiHandler * handler;*/ /* SOFTMPU */
+} midi;
+
+static void PlayMsg(Bit8u* msg,Bitu len)
+{
+	/* Output a MIDI message to the hardware */
+	/* Wait for DRR clear, then output a byte */
+	_asm
+	{
+			mov     bx,msg
+			mov     cx,len                  ; Assume len < 2^16
+			add     cx,bx                   ; Get end ptr
+			mov     dx,midi.mpuport
+	NextByte:       cmp     bx,cx
+			je      End
+			inc     dx                      ; Set cmd port
+	WaitDRR:        in      al,dx
+			test    al,040h
+			jnz     WaitDRR
+			dec     dx                      ; Set data port
+			mov     al,[bx]
+			out     dx,al
+			inc     bx
+			jmp     NextByte
+        End:            nop
+	}
+};
+
+void MIDI_RawOutByte(Bit8u data) {
+        /*if (midi.sysex.start) {
+                Bit32u passed_ticks = GetTicks() - midi.sysex.start;
+                if (passed_ticks < midi.sysex.delay) SDL_Delay(midi.sysex.delay - passed_ticks);
+        }*/ /* TODO */
+
+	/* Test for a realtime MIDI message */
+	if (data>=0xf8) {
+		midi.rt_buf[0]=data;
+		PlayMsg(midi.rt_buf,1);
+		return;
+	}        
+	/* Test for a active sysex tranfer */
+	if (midi.status==0xf0) {
+		if (!(data&0x80)) { 
+			if (midi.sysex.used<(SYSEX_SIZE-1)) midi.sysex.buf[midi.sysex.used++] = data;
+			return;
+		} else {
+			midi.sysex.buf[midi.sysex.used++] = 0xf7;
+
+			if ((midi.sysex.start) && (midi.sysex.used >= 4) && (midi.sysex.used <= 9) && (midi.sysex.buf[1] == 0x41) && (midi.sysex.buf[3] == 0x16)) {
+				/*LOG(LOG_ALL,LOG_ERROR)("MIDI:Skipping invalid MT-32 SysEx midi message (too short to contain a checksum)");*/ /* SOFTMPU */
+			} else {
+				/*LOG(LOG_ALL,LOG_NORMAL)("Play sysex; address:%02X %02X %02X, length:%4d, delay:%3d", midi.sysex.buf[5], midi.sysex.buf[6], midi.sysex.buf[7], midi.sysex.used, midi.sysex.delay);*/
+				PlayMsg(midi.sysex.buf, midi.sysex.used); /* SOFTMPU */
+				if (midi.sysex.start) {
+					if (midi.sysex.buf[5] == 0x7F) {
+						midi.sysex.delay = 290; // All Parameters reset
+					} else if (midi.sysex.buf[5] == 0x10 && midi.sysex.buf[6] == 0x00 && midi.sysex.buf[7] == 0x04) {
+						midi.sysex.delay = 145; // Viking Child
+					} else if (midi.sysex.buf[5] == 0x10 && midi.sysex.buf[6] == 0x00 && midi.sysex.buf[7] == 0x01) {
+						midi.sysex.delay = 30; // Dark Sun 1
+                                        } /*else midi.sysex.delay = (Bitu)(((float)(midi.sysex.used) * 1.25f) * 1000.0f / 3125.0f) + 2;
+                                        midi.sysex.start = GetTicks();*/ /* TODO */
+				}
+			}
+
+			/*LOG(LOG_ALL,LOG_NORMAL)("Sysex message size %d",midi.sysex.used);*/ /* SOFTMPU */
+			/*if (CaptureState & CAPTURE_MIDI) {
+				CAPTURE_AddMidi( true, midi.sysex.used-1, &midi.sysex.buf[1]);
+			}*/ /* SOFTMPU */
+		}
+	}
+	if (data&0x80) {
+		midi.status=data;
+		midi.cmd_pos=0;
+		midi.cmd_len=MIDI_evt_len[data];
+		if (midi.status==0xf0) {
+			midi.sysex.buf[0]=0xf0;
+			midi.sysex.used=1;
+		}
+	}
+	if (midi.cmd_len) {
+		midi.cmd_buf[midi.cmd_pos++]=data;
+		if (midi.cmd_pos >= midi.cmd_len) {
+			/*if (CaptureState & CAPTURE_MIDI) {
+				CAPTURE_AddMidi(false, midi.cmd_len, midi.cmd_buf);
+			}*/ /* SOFTMPU */
+			PlayMsg(midi.cmd_buf,midi.cmd_len);
+			midi.cmd_pos=1;         //Use Running status
+		}
+	}
+}
+
+bool MIDI_Available(void)  {
+	return midi.available;
+}
+
+/* SOFTMPU: Initialisation */
+void MIDI_Init(Bitu mpuport,bool delaysysex)
+{
+        Bitu i; /* SOFTMPU */
+	midi.sysex.delay = 0;
+	midi.sysex.start = 0;
+
+        if (delaysysex==true)
+	{
+		/*midi.sysex.start = GetTicks();*/ /* TODO */
+		/*LOG_MSG("MIDI:Using delayed SysEx processing");*/ /* SOFTMPU */
+	}
+	midi.mpuport=mpuport;
+	midi.status=0x00;
+	midi.cmd_pos=0;
+	midi.cmd_len=0;
+        midi.available=true;
+
+        /* SOFTMPU: Display welcome message on MT-32 */
+        for(i=0;i<30;i++)
+        {
+                MIDI_RawOutByte(MIDI_welcome_msg[i]);
+        }
+}
+
+/* DOSBox initialisation code */
+#if 0
+class MIDI:public Module_base{
+public:
+	MIDI(Section* configuration):Module_base(configuration){
+		Section_prop * section=static_cast<Section_prop *>(configuration);
+		const char * dev=section->Get_string("mididevice");
+		std::string fullconf=section->Get_string("midiconfig");
+		/* If device = "default" go for first handler that works */
+		MidiHandler * handler;
+//              MAPPER_AddHandler(MIDI_SaveRawEvent,MK_f8,MMOD1|MMOD2,"caprawmidi","Cap MIDI");
+		midi.sysex.delay = 0;
+		midi.sysex.start = 0;
+		if (fullconf.find("delaysysex") != std::string::npos) {
+			midi.sysex.start = GetTicks();
+			fullconf.erase(fullconf.find("delaysysex"));
+			LOG_MSG("MIDI:Using delayed SysEx processing");
+		}
+		std::remove(fullconf.begin(), fullconf.end(), ' ');
+		const char * conf = fullconf.c_str();
+		midi.status=0x00;
+		midi.cmd_pos=0;
+		midi.cmd_len=0;
+		if (!strcasecmp(dev,"default")) goto getdefault;
+		handler=handler_list;
+		while (handler) {
+			if (!strcasecmp(dev,handler->GetName())) {
+				if (!handler->Open(conf)) {
+					LOG_MSG("MIDI:Can't open device:%s with config:%s.",dev,conf);
+					goto getdefault;
+				}
+				midi.handler=handler;
+				midi.available=true;    
+				LOG_MSG("MIDI:Opened device:%s",handler->GetName());
+				return;
+			}
+			handler=handler->next;
+		}
+		LOG_MSG("MIDI:Can't find device:%s, finding default handler.",dev);     
+getdefault:     
+		handler=handler_list;
+		while (handler) {
+			if (handler->Open(conf)) {
+				midi.available=true;    
+				midi.handler=handler;
+				LOG_MSG("MIDI:Opened device:%s",handler->GetName());
+				return;
+			}
+			handler=handler->next;
+		}
+		/* This shouldn't be possible */
+	}
+	~MIDI(){
+		if(midi.available) midi.handler->Close();
+		midi.available = false;
+		midi.handler = 0;
+	}
+};
+
+
+static MIDI* test;
+void MIDI_Destroy(Section* /*sec*/){
+	delete test;
+}
+void MIDI_Init(Section * sec) {
+	test = new MIDI(sec);
+	sec->AddDestroyFunction(&MIDI_Destroy,true);
+}
+#endif
+
+/* DOSBox MIDI handler code */
+#if 0
+class MidiHandler;
+
+MidiHandler * handler_list=0;
+
+class MidiHandler {
+public:
+	MidiHandler() {
+		next=handler_list;
+		handler_list=this;
+	};
+	virtual bool Open(const char * /*conf*/) { return true; };
+	virtual void Close(void) {};
+	virtual void PlayMsg(Bit8u * /*msg*/) {};
+	virtual void PlaySysex(Bit8u * /*sysex*/,Bitu /*len*/) {};
+	virtual const char * GetName(void) { return "none"; };
+	virtual ~MidiHandler() { };
+	MidiHandler * next;
+};
+
+MidiHandler Midi_none;
+
+/* Include different midi drivers, lowest ones get checked first for default */
+
+#if defined(MACOSX)
+
+#include "midi_coremidi.h"
+#include "midi_coreaudio.h"
+
+#elif defined (WIN32)
+
+#include "midi_win32.h"
+
+#else
+
+#include "midi_oss.h"
+
+#endif
+
+#if defined (HAVE_ALSA)
+
+#include "midi_alsa.h"
+
+#endif
+#endif /* if 0 */
---
+++ SRC/PIC.C
@@ -0,0 +1,163 @@
+/*
+ *  Copyright (C) 2002-2012  The DOSBox Team
+ *  Copyright (C) 2013       bjt
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * ------------------------------------------
+ * SoftMPU by bjt - Software MPU-401 Emulator
+ * ------------------------------------------
+ *
+ * Based on original pic.c from DOSBox
+ *
+ */
+
+/* SOFTMPU: Moved exported functions & types to header */
+#include "export.h"
+
+void MPU401_Event(void);
+void MPU401_ResetDone(void);
+void MPU401_EOIHandler(void);
+
+/* SOFTMPU: Event countdown timers */
+static Bitu event_countdown[NUM_EVENTS];
+
+/* SOFTMPU: Interrupt management functions */
+void PIC_ActivateIRQ(Bitu sbport)
+{
+        /* Simulate MPU interrupts using a Sound Blaster (or compatible) */
+        _asm
+        {
+                        mov     dx,sbport
+                        add     dx,0Ch                  ; Select DSP command port
+                        mov     al,0F2h                 ; DSP cmd 0xF2 = Force 8-bit IRQ
+                        out     dx,al                   ; Raise IRQ
+        }
+}
+
+void PIC_DeActivateIRQ(Bitu sbport)
+{
+        /* Acknowledge (lower) the SB interrupt by reading the status port */
+        _asm
+        {
+                        mov     dx,sbport
+                        add     dx,0Eh                  ; Select DSP read port
+                        in      al,dx                   ; Acknowledge IRQ
+        }
+}
+
+void PIC_SetIRQMask(Bitu irq, bool masked)
+{
+        const Bit8u t=irq>7?(irq-8):irq;
+        const Bit8u mask=~(1<<t);
+        const Bitu statusport=irq>7?0xA1:0x21;
+
+        _asm
+        {
+                        pushf
+                        cli                             ; No interrupts while progamming PIC
+                        mov     dx,statusport
+                        mov     bl,mask
+                        in      al,dx
+                        and     al,bl                   ; Clear bit
+                        cmp     masked,0
+                        je      SetMask
+                        xor     bl,0FFh                 ; Invert mask
+                        or      al,bl
+        SetMask:        jmp     SHORT $+2               ; Enforce short pause between read & write
+                        out     dx,al                   ; Set new mask
+                        popf                            ; Enable interrupts
+        }
+}
+
+void PIC_AddEvent(EventID event, Bitu delay)
+{
+        /* Dispatch event immediately on zero delay */
+        /* Watch out for blocking loops here... */
+        if (delay==0)
+        {
+                switch (event)
+                {
+                case MPU_EVENT:
+                        /* Don't dispatch immediately as we'll enter an
+                        infinite loop if tempo is high enough */
+                        delay=1; /* Enforce minimum delay */
+                        break;
+                case RESET_DONE:
+                        MPU401_ResetDone();
+                        break;
+                case EOI_HANDLER:
+                        MPU401_EOIHandler();
+                        break;
+                default:
+                        break;
+                }
+        }
+
+        /* SOFTMPU: Set the countdown timer */
+        event_countdown[event]=delay;
+}
+
+void PIC_RemoveEvents(EventID event)
+{
+        /* SOFTMPU: Zero the countdown timer (disable event) */
+        event_countdown[event]=0;
+}
+
+void PIC_Init(void)
+{
+        Bitu i;
+
+        /* SOFTMPU: Zero countdown timers */
+        for (i=0;i<NUM_EVENTS;i++)
+        {
+                PIC_RemoveEvents(i);
+        }
+}
+
+void PIC_Update(void)
+{
+        Bitu i;
+
+        /* SOFTMPU: Decrement countdown timers and dispatch as needed */
+        for (i=0;i<NUM_EVENTS;i++)
+        {
+                if (event_countdown[i] > 0)
+                {
+                        event_countdown[i]--;
+
+                        if (event_countdown[i]==0)
+                        {
+                                /* Dispatch */
+                                switch (i)
+                                {
+                                        case MPU_EVENT:
+                                                MPU401_Event();
+                                                break;
+                                        case RESET_DONE:
+                                                MPU401_ResetDone();
+                                                break;
+                                        case EOI_HANDLER:
+                                                MPU401_EOIHandler();
+                                                break;
+                                        default:
+                                                break;
+                                }
+                        }
+                }
+        }
+}
